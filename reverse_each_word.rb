a9146e0e525e12c3c0a3696c0b9e99bd1dea07db
Mary Selig <seligmar@gmail.com>
2019-06-21 10:05:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,3 @@
-#def reverse_each_word_each(sentence)
- # new_array = []
-  #var1 = sentence.split(" ")
-  #var1.each do |word|
-   # word.reverse 
-    #new_array << word.reverse.join(" ") 
-  #end 
-  #return new_array
-#end
-
 def reverse_each_word(sentence)
   var1 = sentence.split(" ")
   new_sentence = []
